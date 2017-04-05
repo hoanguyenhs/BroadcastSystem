9a6ea6ceee2bd99f0883a98ae1cd8eba82fcb817
HoaNguyen <nguyenthuanhoahs@gmail.com>
2017-04-05 21:47:40 +1200

    Update error at Edit page

--- BroadcastSystem/Models/Message.cs
+++ BroadcastSystem/Models/Message.cs
@@ -11,14 +11,21 @@ namespace BroadcastSystem.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.Web.Mvc;
+
     public partial class Message
     {
         public int ID { get; set; }
         public Nullable<int> AuthorID { get; set; }
+        [DisplayName("Message title")]
         public string Title { get; set; }
+        [DisplayName("Message content")]
+        [AllowHtml]
         public string Content { get; set; }
+        [DisplayName("Broadcasts from")]
         public Nullable<System.DateTime> From { get; set; }
+        [DisplayName("Broadcasts to")]
         public Nullable<System.DateTime> To { get; set; }
         public Nullable<bool> IsBroadcasting { get; set; }
         public Nullable<System.DateTime> CreatedOn { get; set; }
@@ -26,7 +33,7 @@ namespace BroadcastSystem.Models
         public Nullable<System.DateTime> UpdatedOn { get; set; }
         public Nullable<int> UpdatedBy { get; set; }
         public Nullable<bool> IsActived { get; set; }
-    
+
         public virtual Author Author { get; set; }
     }
 }
